10eaf926a71b77cf82e070904c6b5336945aed0e
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-11 15:19:14 +0530

    inerface and type ,union / intersections

--- src/a.ts
+++ src/a.ts
@@ -7,10 +7,72 @@ function add(a: number, b: number): number {
   return a + b;
 }
 greet("Harkirat ");
-//this is how you give type to a function example : callback function 
+//this is how you give type to a function example : callback function
 function runAfter1sec(fn: () => void) {
   setTimeout(fn, 1000);
 }
 runAfter1sec(function () {
   console.log("hi there");
 });
+
+//interface
+interface Person {
+  name: string;
+  age: number;
+  greet(phrase: string): void;
+}
+class Employee implements Person {
+  name: string;
+  age: number;
+
+  constructor(n: string, a: number) {
+    this.name = n;
+    this.age = a;
+  }
+
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}`);
+  }
+}
+const e = new Employee("shorya", 20);
+console.log(e.greet("hi there!"));
+
+//************* interfaces can be implemented as classes but types can't **************//
+
+// Type
+
+type user = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+//extar things type provides
+//unions
+type StringOrNumber = string | number;
+
+function printId(id: StringOrNumber) {
+  console.log(`ID: ${id}`);
+}
+
+printId(101); // ID: 101
+printId("202"); // ID: 202
+
+//intersections
+type Employee2 = {
+  name: string;
+  startDate: Date;
+};
+
+type Manager = {
+  name: string;
+  department: string;
+};
+
+type TeamLead = Employee2 & Manager;
+
+const teamLead: TeamLead = {
+  name: "harkirat",
+  startDate: new Date(),
+  department: "Software developer",
+};
--- typescript-React/src/App.tsx
+++ typescript-React/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Todo title="code" description="do coding" done={true} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
+interface TodoProp {
+  title: string;
+  description: string;
+  done: boolean;
+}
+const Todo = (props: TodoProp) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+      <h3>{props.done ? "Done" : "Not Done"}</h3>
+    </div>
+  );
+};
+export default App;
