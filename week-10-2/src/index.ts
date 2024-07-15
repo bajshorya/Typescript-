6a1cad0a36e03d537d92152d5fa985e1712c902f
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-15 15:37:21 +0530

    insert function made

---
+++ week-10-2/src/index.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function insertUser(
+  username: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      password: true,
+    },
+  });
+  console.log(res);
+}
+insertUser("shorya@baj.com", "pass", "Shorya", "Baj");
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+async function updateUser(
+  username: string,
+  { firstName, lastName }: UpdateParams
+) {}
