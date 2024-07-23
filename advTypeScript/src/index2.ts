9820a2704def2ac3370d8d61ad709c19d5f187ae
SHORYA BAJ <jain.shorya11@gmail.com>
2024-07-24 01:27:19 +0530

    Map

--- advTypeScript/src/index2.ts
+++ advTypeScript/src/index2.ts
@@ -12,3 +12,12 @@ const users: Users = {
 };
 
 console.log(users["abc123"]); // Output: { id: 'abc123', name: 'John Doe' }
+
+//'''''''''''''''''''''''Map''''''''''''''''''''''//
+// another cleaner syntax to get key value pairs
+const User = new Map();
+User.set("baj@1", { name: "Sam", age: 21, email: "sam@gmail.com" });
+User.set("john@1", { name: "John", age: 21, email: "john@gmail.com" });
+
+const singleUser = User.get("baj@1");
+console.log(singleUser);
